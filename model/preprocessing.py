4a14e9c74d4ad6a345462e0e55bedd13177b42a8
chanhee <chk971001@gmail.com>
2022-11-21 09:07:11 +0900

    스탑워즈 추가

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -14,30 +14,54 @@ def cleanText(readData):
 def morp(strings):
    return [w.get_first()+'다' if w.get_second() in ['VV','VA'] else w.get_first() for w in komoran.get_list(cleanText(strings)) if w.get_second() in ['NNP','NNG','MAG','VA','VV','MM']]
 
-df = pd.read_json('./user.json')
+def parsing(df : pd.DataFrame) :
+    titleL = []
+    contentL = []
+    for i in range(len(df)):
+        soup = BeautifulSoup(df['data'][i], "lxml")
+        title = soup.body.h3.text
+        body = []
+        ptags = soup.find('section').find_all('p')
 
-titleL = []
-contentL = []
-# dateL = []
+        for v in ptags:
+            body.append(v.get_text())
+        body = ''.join(body) 
 
-for i in range(len(df)):
-    soup = BeautifulSoup(df['data'][i], "lxml")
-    title = soup.body.h3.text
+        titleL.append(title)
+        contentL.append(body)
+    return dict(title = titleL,
+                content = contentL)
 
-    body = []
-    ptags = soup.find('section').find_all('p')
-    for v in ptags:
-        body.append(v.get_text())
-    body = ''.join(body) 
+if __name__ == "__main__":
+    df = pd.read_json('./user.json')
 
-    titleL.append(title)
-    contentL.append(body)
+    titleL = []
+    contentL = []
 
-resultDict = dict(title = titleL,
-                  content = contentL)
+    for i in range(len(df)):
+        soup = BeautifulSoup(df['data'][i], "lxml")
+        title = soup.body.h3.text
 
-pdf = pd.DataFrame(resultDict)
-pdf = pdf.dropna(subset=['title'])
-pdf = pdf.dropna(subset=['content'])
-pdf['contents'] = pdf.apply(lambda x:x['title'] + "\n" + x['content'], axis=1)
-pdf['all_tokens'] = pdf['contents'].progress_map(lambda x : komoran.get_morphes_by_tags(cleanText(x)))
+        body = []
+        ptags = soup.find('section').find_all('p')
+        for v in ptags:
+            body.append(v.get_text())
+        body = ''.join(body) 
+
+        titleL.append(title)
+        contentL.append(body)
+
+    resultDict = dict(title = titleL,
+                    content = contentL)
+
+    pdf = pd.DataFrame(resultDict)
+    pdf = pdf.dropna(subset=['title'])
+    pdf = pdf.dropna(subset=['content'])
+    pdf['contents'] = pdf.apply(lambda x:x['title'] + "\n" + x['content'], axis=1)
+    pdf['all_tokens'] = pdf['contents'].progress_map(lambda x : komoran.get_morphes_by_tags(cleanText(x)))
+
+    with open('./model/stopwords/stopwords.txt') as f:
+        stopwordsList = f.read().splitlines()
+    stopwordsSet = set(stopwordsList)
+    pdf['all_tokens'] = pdf['all_tokens'].map(lambda x:[w for w in x if not w in stopwordsSet])
+
